deaa821152d7160cf65961db540273c8db185ee9
Gen Student1 <student1@student.touro.edu>
2016-02-04 20:38:48 -0500

    Marvin Project

---
+++ src/main/java/glazer/mco364/todo/TodoService.java
@@ -0,0 +1,12 @@
+package glazer.mco364.todo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface TodoService {
+
+	@GET("/todos")
+	Call<List<Todo>> listTodos();
+}
